0d6f7595069ebfc035fccfc0ee6c10a925c62129
ItzLegendBoi <harsh637790@gmail.com>
2026-06-17 14:28:45 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,33 @@
+num_str = "123"
+num_int = int(num_str)
+print(num_str)
+#-----------Next code------------
+
+a = 9
+b = 6.5
+result = a + b
+print("Result:",result)
+print("Type of result:", type(result))
+#---------Next code------------------
+
+x = int("1001",2)   #string to integer
+print(x)
+y = float("10")     #string to float
+print(y)
+print(ord('A'))     #into a ascii
+print(hex(555))     #integer to hexadecimal
+print(oct(9))       #integer to octal
+#---------------Next Code-------------------
+#Tuple()
+print(tuple("harsh"))
+print(tuple([1,2,3]))
+#list()
+print(list("harsh"))
+#From a tuple
+print(list([1,2,3]))
+#No parameter
+print(list())
+#Set
+print(set[1,2,3,3,4])
+#No parameter
+print(set())
